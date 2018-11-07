dc940a99daf3b4f486b2ad0c13c8ae830b865993
haiden <0411hudeng@163.com>
2018-11-08 00:31:10 +0800

    加入model

--- MyJava/src/com/huawei/User.java
+++ MyJava/src/com/huawei/User.java
@@ -1,33 +1,9 @@
 package com.huawei;
 
-public class User {
-    private Integer age;
-    private String name;
-    private String password;
+import com.jfinal.plugin.activerecord.Model;
 
-    public Integer getAge() {
-        return age;
-    }
+public class User extends Model<User> {
 
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
+    public static  final User dao = new User();
 
 }
